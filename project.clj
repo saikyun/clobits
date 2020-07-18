a468d3cdc48a1e6bc9c7e0ed71280ec2e3ce9596
Jona Ekenberg <saikyun@gmail.com>
2020-07-18 13:00:15 +0200

    removed old test code, restructured project.clj

--- project.clj
+++ project.clj
@@ -8,47 +8,35 @@
   :resources ["libs"]
   
   :clean-targets [:target-path "examples" "libs"]
-
+  
   :prep-tasks [["with-profile" "compile-clobits" "compile"]]
   
-  :profiles {:compare-files {:dependencies [[digest "1.4.9"]]
-                             :source-paths ["test-src"]}
+  :profiles {:compile-clobits {:prep-tasks ^:replace []
+                               :aot [clobits.wrappers]}
              
-             :compile-clobits {:prep-tasks ^:replace []
-                               :aot [clobits.wrappers]
-                               ;;:compile-path "target/classes/"
-                               }
+             :socket {:jvm-opts ["-Dclojure.server.repl={:port 5555 :accept clojure.core.server/repl}"]}
              
-             :gen-sdl {:prep-tasks ^:replace []
-                       :main clobits.examples.sdl.create-sdl-lib}
+             
+             
+             ;; ncurses
              
              :gen-ncurses {:prep-tasks ^:replace []
                            :main clobits.examples.ncurses.create-ncurses-lib}
-             
-             :compile-java {:java-source-paths ["examples/src/java"]
-                            :aot :all}
-             
-             :compile-sdl {:prep-tasks [["with-profile" "gen-sdl" "run"] "compile"]
-                           :aot [clobits.sdl.structs clobits.sdl.ni.generate]
-                           :java-source-paths ["examples/src/java"]
-                           :source-paths ["examples/src/clj"]}             
              
              :compile-ncurses {:prep-tasks [["with-profile" "gen-ncurses" "run"] "compile"]
                                :aot [clobits.ncurses.ni.generate]
                                :java-source-paths ["examples/src/java"]
                                :source-paths ["examples/src/clj"]}
              
-             :uberjar {:jvm-opts ["-Dclojure.compiler.direct-linking=true"
-                                  "-Dclojure.spec.skip-macros=true"]}
              
-             :sdl-uberjar {:prep-tasks [["with-profile" "compile-sdl" "compile"]
-                                        "javac"
-                                        "compile"]
-                           :source-paths ["examples/src/clj"]
-                           :java-source-paths ["examples/src/java"]
-                           :uberjar-name "examples_sdl.jar"
-                           :main clobits.examples.sdl.startup
-                           :aot [clobits.examples.sdl.startup]}
+             :ncurses-poly {:prep-tasks [["with-profile" "compile-ncurses" "compile"]]
+                            :main clobits.examples.ncurses.startup
+                            :source-paths ["examples/src/clj"]}             
+             
+             :bounce-poly {:prep-tasks [["with-profile" "compile-ncurses" "compile"]]
+                           :main clobits.examples.ncurses.bounce-startup
+                           :source-paths ["examples/src/clj"]}             
+             
              
              :ncurses-uberjar {:prep-tasks [["with-profile" "compile-ncurses" "compile"]
                                             "javac"
@@ -63,23 +51,42 @@
                                            "compile"]
                               :source-paths ["examples/src/clj"]
                               :uberjar-name "examples_bounce.jar"
-                              :main clobits.examples.ncurses.bounce-startup                              
+                              :main clobits.examples.ncurses.bounce-startup
                               :aot [clobits.examples.ncurses.bounce-startup]}
+             
+             
+             
+             ;; SDL
+             
+             :gen-sdl {:prep-tasks ^:replace []
+                       :main clobits.examples.sdl.create-sdl-lib}
+             
+             :compile-sdl {:prep-tasks [["with-profile" "gen-sdl" "run"] "compile"]
+                           :aot [clobits.sdl.structs clobits.sdl.ni.generate]
+                           :java-source-paths ["examples/src/java"]
+                           :source-paths ["examples/src/clj"]}
+             
              
              :sdl-poly {:prep-tasks [["with-profile" "compile-sdl" "compile"]]
                         :main clobits.examples.sdl.startup
                         :source-paths ["examples/src/clj"]}
              
-             :ncurses-poly {:prep-tasks [["with-profile" "compile-ncurses" "compile"]]
-                            :main clobits.examples.ncurses.startup
-                            :source-paths ["examples/src/clj"]}
              
-             :bounce-poly {:prep-tasks [["with-profile" "compile-ncurses" "compile"]]
-                           :main clobits.examples.ncurses.bounce-startup
-                           :source-paths ["examples/src/clj"]}
+             :sdl-uberjar {:prep-tasks [["with-profile" "compile-sdl" "compile"]
+                                        "javac"
+                                        "compile"]
+                           :source-paths ["examples/src/clj"]
+                           :java-source-paths ["examples/src/java"]
+                           :uberjar-name "examples_sdl.jar"
+                           :main clobits.examples.sdl.startup
+                           :aot [clobits.examples.sdl.startup]}             
              
              
-             :macos {:jvm-opts [;; sdl function regarding the window / renderer
+             
+             
+             ;; ni build config
+             
+             :macos {:jvm-opts [;; sdl functions using the window / renderer
                                 ;; can only be run on first thread on macos
                                 "-XstartOnFirstThread"
                                 ;; needed to find libSDL2 on macos
@@ -88,5 +95,7 @@
              :linux {:jvm-opts [;; needed to find libSDL2 on linux       
                                 "-Djava.library.path=/usr/lib/x86_64-linux-gnu"]}
              
-             :clojure-1.10.2-alpha1 {:dependencies [[org.clojure/clojure "1.10.2-alpha1"]]}
-             :socket {:jvm-opts ["-Dclojure.server.repl={:port 5555 :accept clojure.core.server/repl}"]}})
+             :uberjar {:jvm-opts ["-Dclojure.compiler.direct-linking=true"
+                                  "-Dclojure.spec.skip-macros=true"]}             
+             
+             :clojure-1.10.2-alpha1 {:dependencies [[org.clojure/clojure "1.10.2-alpha1"]]}})
